896f76113e0e70d85f7396ff109e349e7701b00a
Charles Ellis <hamled@hamled.name>
2018-11-09 11:09:05 -0800

    Adagrams.scoreWord() tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -10,6 +10,18 @@ const Adagrams = {
     12: 'E',
   },
 
+  SCORE_CHART: {
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'D': 2, 'G': 2,
+    'B': 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5,
+    'J': 8, 'X': 8,
+    'Q': 10, 'Z': 10,
+  },
+
+  LENGTH_BONUS: 8,
+
   drawLetters() {
     const letterPool = Object.entries(Adagrams.LETTER_COUNTS).reduce((letters, [count, letterSet]) => {
       return letters + letterSet.repeat(count);
@@ -35,6 +47,17 @@ const Adagrams = {
       delete drawnCopy[index];
       return true;
     });
+  },
+
+  scoreWord(word) {
+    return word.toUpperCase().split('').reduce((wordScore, letter) => {
+      const letterScore = Adagrams.SCORE_CHART[letter];
+      if(letterScore === undefined) {
+        throw `${letter} is not in the English alphabet!`;
+      }
+
+      return wordScore + letterScore;
+    }, word.length < 7 ? 0 : Adagrams.LENGTH_BONUS);
   },
 };
 
