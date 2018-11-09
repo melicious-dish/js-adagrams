ec0aaa64c2767f8773bdbb95123f3ccd15d3371f
Charles Ellis <hamled@hamled.name>
2018-11-09 11:07:52 -0800

    Adagrams.drawLetters() tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,27 @@
 const Adagrams = {
+  LETTER_COUNTS: {
+    1: 'JKQXZ',
+    2: 'BCFHMPVWY',
+    3: 'G',
+    4: 'DLSU',
+    6: 'NRT',
+    8: 'O',
+    9: 'AI',
+    12: 'E',
+  },
+
   drawLetters() {
-    // Implement this method for wave 1
+    const letterPool = Object.entries(Adagrams.LETTER_COUNTS).reduce((letters, [count, letterSet]) => {
+      return letters + letterSet.repeat(count);
+    }, '').split('');
+
+    let letters = [];
+    for(let i = 0; i < 10; i++) {
+      const index = Math.floor(Math.random() * letterPool.length);
+      letters.push(letterPool.splice(index, 1)[0]); // Remove entry from letter_pool at index, add to letters array
+    }
+
+    return letters;
   },
 };
 
